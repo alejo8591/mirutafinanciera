12aecbc0991121362979499fdea55264c10d00a6
Alejandro Romero <alejo8591@gmail.com>
2013-11-23 20:08:53 -0500

    update REST API

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.post('/adduser', console.log(us.addUser));
+app.post('/adduser', function(req, res){
+	console.log(req.body.email);
+	//us.addUser(req.param.email, req.param.name);
+});
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
